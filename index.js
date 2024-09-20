69352349ca399da3cc6446db08739b11be5f9021
alexchtst-01 <acinatra@gmail.com>
2024-09-20 15:42:40 +0700

    feat: update index.js entry-point and login routes

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import morgan from "morgan";
 
 import productRoute from "./src/routes/productRoute.js";
 import userRoute from "./src/routes/userRoute.js";
+import loginRoute from "./src/routes/loginRoute.js";
 
 dotenv.config();
 
 const server = express();
+server.use(cors());
 server.use(express.json());
-server.use(morgan("combined"));
-
-server.use("/api/v1", userRoute);
-server.use("/api/v1", productRoute);
 
 server.get("/", (req, res) => {
   res.send("Backend server PAW kelompok 5");
 });
 
+server.use("/api/v1", loginRoute);
+server.use("/api/v1", userRoute);
+server.use("/api/v1", productRoute);
+
+server.use(morgan("dev"));
+
 mongoose
-  .connect(process.env.CON_STRING)
+  .connect(process.env.MONGO_STRING)
   .then(() => {
     console.log("Connected to MongoDB Atlas");
     server.listen(process.env.SERVER_PORT, () => {
